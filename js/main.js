bf46fe5541e9039ec52587d64556a99242094e20
sumanofficial <officialofsuman@gmail.com>
2021-06-23 17:06:57 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$("#home-banner").slick({
+  autoplay: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: "linear",
+  arrows: false,
+});
+$(".home-update-banner").slick({
+  infinite: true,
+  autoplay: true,
+  speed: 100,
+  fade: true,
+  cssEase: "linear",
+  arrows: false,
+});
+$(".footer-slider").slick({
+  infinite: true,
+  autoplay: true,
+  speed: 300,
+  fade: true,
+  cssEase: "linear",
+  arrows: false,
+});
