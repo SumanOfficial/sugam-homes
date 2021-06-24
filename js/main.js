582b66938f2fb0334ad8bc8bc153ef0ed868e317
sumanofficial <officialofsuman@gmail.com>
2021-06-24 23:52:24 +0530

    sugam habitat

--- js/main.js
+++ js/main.js
@@ -22,3 +22,28 @@ $(".footer-slider").slick({
   cssEase: "linear",
   arrows: false,
 });
+$(".urban_lakes_banner").slick({
+  infinite: true,
+  autoplay: true,
+  speed: 300,
+  fade: true,
+  cssEase: "linear",
+  arrows: false,
+});
+
+$(document).ready(function () {
+  $(".amenities-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    },
+  });
+});
